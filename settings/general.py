c02bafaaf108446b62fd5c50cf8902eaee7923e5
Rik <rik@basegrow.com>
2013-06-24 21:12:02 +0200

    reordered files

--- settings/general.py
+++ settings/general.py
@@ -107,6 +107,7 @@ ROOT_URLCONF = 'urls'
 WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
+    os.path.join(DJANGO_ROOT, 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
