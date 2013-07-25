478c0a6559ec44a62918699c3fa221925f2211aa
Rik <rik@basegrow.com>
2013-07-25 20:38:52 +0200

    same boxes auto change along with first chord

--- static/js/apps/chordcharts/edit.js
+++ static/js/apps/chordcharts/edit.js
@@ -10,12 +10,48 @@ $(function() {
     // BoxPart
 
     BoxedChart.Models.BoxPart = Backbone.Model.extend({
+
+        initialize: function(attributes) {
+            this.listenTo(this, 'change', this.change)
+        },
+
+        change: function() {
+            // If this and the next box have beat_schema '4' and they're both
+            // the same, also change the chord on the next box.
+
+            if(
+                this.get('beats') == 4 &&
+                this.get('box').has('next_box') &&
+                this.get('box').get('next_box').get('beat_schema') == '4'
+            ) {
+
+                var next_box_part = this.get('box').get('next_box')
+                    .get('parts').first()
+                var last_attributes = this.previousAttributes()
+
+                if(
+                    _.isEqual(next_box_part.get('note'), last_attributes.note) &&
+                    _.isEqual(next_box_part.get('chord_type'), last_attributes.chord_type) &&
+                    _.isEqual(next_box_part.get('alt_base_note'), last_attributes.alt_base_note)
+                ) {
+                    this.get('box').get('next_box').get('parts').first().set({
+                        'note': this.get('note'),
+                        'chord_type': this.get('chord_type'),
+                        'alt_base_note': this.get('alt_base_note')
+                    })
+                }
+
+            }
+
+        },
+
         chordName: function() {
             return (
                 this.get('note').name +
                 this.get('chord_type').symbol
             )
         }
+
     })
 
     BoxedChart.Collections.BoxPart = Backbone.Collection.extend({
@@ -27,7 +63,7 @@ $(function() {
         model: BoxedChart.Models.BoxPart,
 
         initialize: function() {
-            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'change', this.render)
         },
 
         events: {
@@ -176,7 +212,7 @@ $(function() {
         model: BoxedChart.Models.editWidget,
 
         initialize: function() {
-            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'change', this.render)
         },
 
         events: {
