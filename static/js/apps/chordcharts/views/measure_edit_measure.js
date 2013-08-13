e8e2bbdad55b8c32f722cb9ece12519c259d02ff
Rik <rik@basegrow.com>
2013-08-14 00:17:56 +0200

    measure edit updates

--- static/js/apps/chordcharts/views/measure_edit_measure.js
+++ static/js/apps/chordcharts/views/measure_edit_measure.js
@@ -5,16 +5,124 @@ define(
         return Backbone.View.extend({
 
             model: MeasureEditMeasure,
+            tagName: 'li',
 
             render: function() {
 
                 this.$el.html(
-                    '<li>' +
-                        '<div class="measure measure-beatschema-' + this.model.get('beat_schema') + '"></div>' +
-                    '</li>'
+                    '<div class="measure measure-beatschema-' +
+                        this.model.get('beat_schema') +
+                    '"></div>'
                 )
 
+                this.drawSeperationLines()
+
                 return this
+
+            },
+
+            drawSeperationLines: function() {
+
+                var element = this.$el.find('.measure')
+
+                switch(this.model.get('beat_schema')) {
+
+                    case '2-2':
+
+                        var canvas = document.createElement('canvas')
+                        var context = canvas.getContext('2d')
+
+                        canvas.style.position = 'absolute'
+                        canvas.width = 50
+                        canvas.height = 50
+
+                        context.lineWidth = 1
+
+                        context.beginPath()
+                        context.moveTo(50, 0)
+                        context.lineTo(0, 50)
+                        context.stroke()
+
+                        element.prepend(canvas)
+
+                        break
+
+                    case '2-1-1':
+
+                        var canvas = document.createElement('canvas')
+                        var context = canvas.getContext('2d')
+
+                        canvas.style.position = 'absolute'
+                        canvas.width = 50
+                        canvas.height = 50
+
+                        context.lineWidth = 1
+
+                        context.beginPath()
+                        context.moveTo(50, 0)
+                        context.lineTo(0, 50)
+                        context.stroke()
+
+                        context.beginPath()
+                        context.moveTo(25, 25)
+                        context.lineTo(50, 50)
+                        context.stroke()
+
+                        element.prepend(canvas)
+
+                        break
+
+                    case '1-1-2':
+
+                        var canvas = document.createElement('canvas')
+                        var context = canvas.getContext('2d')
+
+                        canvas.style.position = 'absolute'
+                        canvas.width = 50
+                        canvas.height = 50
+
+                        context.lineWidth = 1
+
+                        context.beginPath()
+                        context.moveTo(50, 0)
+                        context.lineTo(0, 50)
+                        context.stroke()
+
+                        context.beginPath()
+                        context.moveTo(0, 0)
+                        context.lineTo(25, 25)
+                        context.stroke()
+
+                        element.prepend(canvas)
+
+                        break
+
+                    case '1-1-1-1':
+
+                        var canvas = document.createElement('canvas')
+                        var context = canvas.getContext('2d')
+
+                        canvas.style.position = 'absolute'
+                        canvas.width = 50
+                        canvas.height = 50
+
+                        context.lineWidth = 1
+
+                        context.beginPath()
+                        context.moveTo(50, 0)
+                        context.lineTo(0, 50)
+                        context.stroke()
+
+                        context.beginPath()
+                        context.moveTo(0, 0)
+                        context.lineTo(50, 50)
+                        context.stroke()
+
+                        element.prepend(canvas)
+
+                        break
+
+                }
 
             }
 
