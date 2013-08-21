b7fbdb48412c2ca395040862bdffcce0c7d60b10
Rik <gitaarik@gmail.com>
2013-08-21 11:41:51 +0200

    set --nostatic option by default in manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,14 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+if __name__ == '__main__':
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
     from django.core.management import execute_from_command_line
+
+    # Disable django.contrib.staticfiles from serving the static files, because
+    # we serve the static files ourselfs with nocache headers.
+    if '--nostatic' not in sys.argv:
+        sys.argv.append('--nostatic')
+
     execute_from_command_line(sys.argv)
