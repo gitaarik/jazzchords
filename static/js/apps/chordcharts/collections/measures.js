c15acce70f3a2357b819bfad585024f28422e0ad
Rik <gitaarik@gmail.com>
2013-08-26 21:08:58 +0200

    Revert "made most models 'smart' with listeners"
    This reverts commit 4e7296f657bac949db4c222778b2459885742607.

--- static/js/apps/chordcharts/collections/measures.js
+++ static/js/apps/chordcharts/collections/measures.js
@@ -7,8 +7,28 @@ define(
             model: Measure,
 
             delete: function(measure) {
+
+                var prev_measure = measure.get('prev_measure')
+                var next_measure = measure.get('next_measure')
+
+                if(prev_measure) {
+
+                    if(next_measure) {
+                        next_measure.set({
+                            'prev_measure': prev_measure,
+                            'number': measure.get('number')
+                        })
+                        prev_measure.set('next_measure', next_measure)
+                    }
+                    else {
+                        prev_measure.unset('next_measure')
+                    }
+
+                }
+
                 this.remove(measure)
                 measure.destroy()
+
             }
 
         })
