9d1f64f5d96d46fb29eae7672e500e06c5392d58
Rik <gitaarik@gmail.com>
2014-05-16 13:44:58 +0200

    some stuff

--- static/js/apps/chordcharts/views/subsection_sidebar_part_edit.js
+++ static/js/apps/chordcharts/views/subsection_sidebar_part_edit.js
@@ -72,6 +72,49 @@ define(
                 console.log('choosing letter:');
                 console.log(letter);
 
+                /*
+
+                If the user changes the letter of a line inside a
+                subsection, then do this:
+
+                - If this line is the only line in the subsection and:
+                  If there is no preceding subsection or a preceding
+                  subsection with a different letter than the chosen
+                  one and:
+                  If there is no following subsection or a following
+                  subsection with a different letter than the chosen
+                  one:
+                    - Set the subsection's letter to the chosen letter.
+                - Else:
+
+                    - If this line has a preceding line:
+                        - If this line has a following line:
+                            - Upper `number` of following subsections.
+                            - Create new subsection where `number` is
+                              preceding subsection's number + 1.
+                            - Move this line to the new subsection.
+                        - Else, if this subsection has a following
+                          subsection:
+                            - 
+
+                    - Else, if this subsection has a preceding
+                      subsection and it's letter is the same as the
+                      chosen letter:
+                        - Move the line to the end of the preceding
+                          subsection.
+                        - If this subsection has lines left:
+                            - Lower the `number` of the lines.
+                        - Else:
+                            - Remove this subsection
+                            - Lower the `number` of the following
+                              subsections.
+
+                    - If this line has a following line:
+
+                Bye bye subsections....?
+
+                */
+
             }
 
         });
