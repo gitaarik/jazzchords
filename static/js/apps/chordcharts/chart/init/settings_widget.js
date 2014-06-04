dfb92fb41f3f42887b2a7734214982cd8f20cbfe
Rik <gitaarik@gmail.com>
2014-06-04 18:09:54 +0200

    removed whitespace

--- static/js/apps/chordcharts/chart/init/settings_widget.js
+++ static/js/apps/chordcharts/chart/init/settings_widget.js
@@ -12,9 +12,7 @@ define(
         widget_el.find('.sub-buttons .sub-button.delete').click(function() {
 
             if (confirm("Are you really sure you want to delete the chart?")) {
-
                 $(this).find('form').submit();
-
             }
 
         });
