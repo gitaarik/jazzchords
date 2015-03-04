63c0440ff2435a605d585db2cd441da71f14b719
Rik <gitaarik@gmail.com>
2015-03-04 09:38:33 +0100

    created base data dump management command

--- apps/core/management/commands/base_data_dump.py
+++ apps/core/management/commands/base_data_dump.py
@@ -3,17 +3,19 @@ from django.core import management
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        db_dump_file = os.path.join(settings.DJANGO_ROOT, 'dev/db-dump.json')
+        db_dump_file = os.path.join(settings.DJANGO_ROOT, 'dev/base-data-dump.json')
 
         with open(db_dump_file, 'w') as output_file:
             management.call_command(
                 'dumpdata',
-                'chordcharts',
-                'songs',
-                'users',
+                'chordcharts.Key',
+                'chordcharts.Note',
+                'chordcharts.ChordType',
+                'chordcharts.TimeSignature',
                 stdout=output_file
             )
