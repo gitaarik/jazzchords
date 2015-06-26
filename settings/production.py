cbe3d0291461fe7e15aaa01887e4651ec2800d06
Rik <gitaarik@gmail.com>
2015-06-26 02:40:43 +0200

    settings production - using elasticsearch for haystack

--- settings/production.py
+++ settings/production.py
@@ -1,13 +1,10 @@
-from .general import INSTALLED_APPS
-
-
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jazzchords',                      
+        'NAME': 'jazzchords',
         'USER': 'jazzchords',
         'PASSWORD': 's3cRut@s$H!t',
         'HOST': 'localhost'
@@ -19,8 +16,8 @@ WEBSITE_URL = 'http://www.{}'.format(DOMAIN_NAME)
 
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        'PATH': os.path.join(DEV_ROOT, 'whoosh_index'),
-        'INCLUDE_SPELLING': True
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
     },
 }
